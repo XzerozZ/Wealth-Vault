442d51d66531d6d564c991b348312174d7cba3a2
XzerozZ <111548296+XzerozZ@users.noreply.github.com>
2026-01-14 14:04:40 +0700

    UPDATE User data and Asset Structure

--- gen-proto.ps1
+++ gen-proto.ps1
@@ -57,4 +57,19 @@ protoc `
   --go-grpc_out=api-gateway/pkg/pb --go-grpc_opt=paths=source_relative `
   proto/user/user.proto
 
+# =========================
+# ASSET PROTO
+# =========================
+Write-Host "Generating Asset Proto..."
+protoc `
+  --proto_path=. `
+  --go_out=services/asset-service/pkg/pb --go_opt=paths=source_relative `
+  --go-grpc_out=services/asset-service/pkg/pb --go-grpc_opt=paths=source_relative `
+  proto/asset/asset.proto
+
+protoc `
+  --proto_path=. `
+  --go_out=api-gateway/pkg/pb --go_opt=paths=source_relative `
+  --go-grpc_out=api-gateway/pkg/pb --go-grpc_opt=paths=source_relative `
+  proto/asset/asset.proto
 Write-Host "Protobuf generated successfully"
